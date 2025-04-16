855b0ff3ffed01c45281c51e5611d763a8d95972
somefan0102 <rntangiri@gmail.com>
2025-04-16 00:27:56 -0400

    204-205, 210: Added!

---
+++ src/chapter2/exer204-205.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+void squeeze(char[], char[]);
+
+int main(void) {
+    char string1[] = "Hello, world!";
+    char string2[] = "C is a very cool and incredible language!";
+    char string3[] = "Lorem ipsum dolor sit amet.";
+
+    squeeze(string1, "lo");
+    squeeze(string2, "Cce!");
+    squeeze(string3, "aeiou");
+
+    printf("%s\n%s\n%s", string1, string2, string3);
+    return 0;
+}
+
+void squeeze(char string1[], char string2[]) {
+    int i, j;
+
+    for (int k = 0; string2[k] != '\0'; k++) {
+        for (i = 0, j = 0; string1[i] != '\0'; i++) {
+            if (string1[i] != string2[k]) {
+                string1[j++] = string1[i];
+            }
+        }
+        string1[j] = '\0';
+    }
+}
+
+/*
+204: Write an alternative version of `squeeze(s1, s2)` that deletes each character
+     in `s1` that matches any character in the string `s2`.
+205: Write the function any(s1,s2), which returns the first location in a string s1
+     where any character from the string s2 occurs, or -1 if s1 contains no characters from s2.
+     (The standard library function strpbrk does the same job but returns a pointer to the location.)
+*/
---
+++ src/chapter2/exer210.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+char lower(char);
+void lowerString(char[]);
+
+int main(void) {
+    char message[] = "IT WORKS!";
+    lowerString(message);
+    printf(message);
+    return 0;
+}
+
+void lowerString(char string[]) {
+    for (int i = 0; string[i] != '\0'; i++) {
+        string[i] = lower(string[i]);
+    }
+}
+
+char lower(char ch) {
+    return (ch >= 'A' && ch <= 'Z') ? (ch + 'a' - 'A') : ch;
+}
+
+/*
+210: Rewrite the function lower, which converts upper case letters to lower case,
+     with a conditional expression instead of if-else.
+*/
